bdea55269eda4cab5046006e3512f0169e140906
ICBmasterchief <ignacioelincreible@hotmail.com>
2025-05-30 20:36:33 +0200

    Add createdAt and updatedAt fields to users and swipes tables migrations

--- backend/src/migrations/20250402121552-create-swipe-table.cjs
+++ backend/src/migrations/20250402121552-create-swipe-table.cjs
@@ -4,37 +4,24 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      "Users",
+      "Swipes",
       {
         id: {
           type: Sequelize.INTEGER,
           autoIncrement: true,
           primaryKey: true,
         },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        age: {
+        userId: {
           type: Sequelize.INTEGER,
-          allowNull: true,
+          allowNull: false,
         },
-        gender: {
-          type: Sequelize.ENUM("hombre", "mujer"),
-          allowNull: true,
+        targetUserId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
         },
-        photoUrl: {
-          type: Sequelize.STRING,
-          allowNull: true,
+        liked: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
         },
         createdAt: {
           type: Sequelize.DATE,
@@ -52,6 +39,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable("Users");
+    await queryInterface.dropTable("Swipes");
   },
 };
